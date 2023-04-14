4a42df05c0fb25e3f06a8db6da20da60cf83596a
hongj0a <52265361+hongj0a@users.noreply.github.com>
2023-04-14 17:43:13 +0900

    updated for all_api_protoType

---
+++ src/main/java/dejay/rnd/billyG/dto/ReviewDto.java
@@ -0,0 +1,13 @@
+package dejay.rnd.billyG.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OutDto {
+   private Long typeIdx;
+   private String blockContent;
+}
